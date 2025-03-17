4c57ca62c92a4767a736deff7c06ec253e52cff9
Siravit Phokeed <siravitsasa@gmail.com>
2025-03-17 23:29:09 +0700

    fix: IntaniaLocation requires room

--- lib/models/IntaniaLocation.ts
+++ lib/models/IntaniaLocation.ts
@@ -9,16 +9,16 @@ export default class IntaniaLocation {
   /** The building name, i.e. 2, iCanteen, etc. Omit the word building. */
   #building: string;
 
-  constructor(room: string, floor: string, building: string) {
+  constructor(room: string | null, floor: string, building: string) {
     this.#room = room;
     this.#floor = floor;
     this.#building = building;
   }
 
   /** Formats the location as a string. */
-  get string() {
+  get text() {
     return [
-      ...(this.#room ? [SEPARATOR] : []),
+      ...(this.#room ? [this.#room, SEPARATOR] : []),
       `ตึก ${this.#building}`,
       `ชั้น ${this.#floor}`,
     ].join(" ");
