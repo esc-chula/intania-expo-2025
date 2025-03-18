e6576c7445207e9dae30a481889e13d57cb4ec76
Siravit Phokeed <siravitsasa@gmail.com>
2025-03-19 01:36:03 +0700

    feat: TopAppBar

--- app/components/TopAppBar.tsx
+++ app/components/TopAppBar.tsx
@@ -1,34 +1,33 @@
-import Icon from "@/app/components/Icon";
 import cn from "@/lib/helpers/cn";
 import { StyleableFC } from "@/lib/types/misc";
 
 /**
- * Top App Bar displays the page name and a back button to navigate up.
+ * Top App Bar displays the page name, a back button to navigate up, and related
+ * actions.
  *
  * @param children The name of the page.
- * @param parentURL The URL to navigate up to.
- *
- * @note A temporary example to what a component can look like.
+ * @param appearance Whether the background is fully filled or fading scrim.
  */
 const TopAppBar: StyleableFC<{
   children: React.ReactNode;
-  parentURL?: string;
-}> = ({ children, parentURL, className, style }) => (
+  appearance?: "filled" | "scrim";
+}> = ({ children, appearance = "filled", className, style }) => (
   <header
     className={cn(
-      `bg-dark-red fixed top-0 flex h-16 w-screen max-w-108 items-center gap-4
-      px-4 text-white`,
+      `text-headline-sm leading-headline-sm fixed top-0 flex h-16 w-screen
+      max-w-108 items-center gap-2 px-2 text-white [&_h1]:grow
+      [&_h1]:truncate [&_h1]:font-bold`,
+      {
+        filled: `bg-dark-red`,
+        scrim: `*:z-10 before:absolute before:inset-0 before:h-30
+          before:bg-gradient-to-b before:from-black before:to-transparent
+          before:content-['']`,
+      }[appearance],
       className,
     )}
     style={style}
   >
-    {parentURL && <Icon name="arrow_back" />}
-    <h1
-      className={cn(`text-headline-sm leading-headline-sm grow truncate
-        font-bold`)}
-    >
-      {children}
-    </h1>
+    {children}
   </header>
 );
 
