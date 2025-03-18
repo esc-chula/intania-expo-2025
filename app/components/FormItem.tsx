cf0feb56c8a35a18e650d130fc4eb9be84a7a10f
MildlyBrain <maethaphan.phai@gmail.com>
2025-03-19 01:37:36 +0700

    feat: add FormItem component

---
+++ app/components/FormItem.tsx
@@ -0,0 +1,24 @@
+/**
+ * Form Item displays the form label and corresponding input field.
+ *
+ * @param children The input field (Field/Select), displays on the right.
+ * @param label The form label, displays on the left.
+ *
+ */
+
+export default function FormItem({
+  children,
+  label,
+}: {
+  children: React.ReactNode;
+  label: string;
+}) {
+  return (
+    <div className="mx-4 my-3 flex h-13 items-center justify-between text-white">
+      <span className="w-3/10 self-center pr-5 text-right text-base font-semibold italic">
+        {label}
+      </span>
+      <div className="grow self-center content-center h-full">{children}</div>
+    </div>
+  );
+}
