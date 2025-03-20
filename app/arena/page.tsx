cc6378dac64f97a9f74f7a21d196341c34470406
agehcx <chega.malee@gmail.com>
2025-03-20 19:02:03 +0700

    fix: remove motion, replace button, fix indent

--- app/arena/page.tsx
+++ app/arena/page.tsx
@@ -1,10 +1,50 @@
 import EventCard from "../components/Arena/EventCard";
-import eventData from "../components/Arena/eventData";
 import Button from "../components/Button";
 import Icon from "../components/Icon";
 import TopAppBar from "../components/TopAppBar";
 
 export default function Events() {
+    const eventData = [
+        {
+            title: "Intania Hackathon",
+            description:
+                "เตรียมตัวพบกับ Intania Hackathon​ - งานที่จะสร้าง บรรยากาศที่ทำให้ทุกคนได้สร้างสิ่งใหม่ ๆ แก้ไขปัญหาใหม่ ๆ ที่สร้าง Impact ได้จริงไปด้วยกัน",
+            image: "/arena/ih.png",
+            links: [
+                { name: "ชมถ่ายทอดสด", url: "/" },
+                { name: "ติดตาม", url: "/" },
+            ],
+        },
+        {
+            title: "Integration Bee Challenge",
+            description:
+                "เตรียมตัวพบกับ Intania Hackathon​ - งานที่จะสร้าง บรรยากาศที่ทำให้ทุกคนได้สร้างสิ่งใหม่ ๆ แก้ไขปัญหาใหม่ ๆ ที่สร้าง Impact ได้จริงไปด้วยกัน",
+            image: "/arena/ib.png",
+            links: [
+                { name: "สมัคร", url: "/" },
+                { name: "อ่านต่อ", url: "/" },
+            ],
+        },
+        {
+            title: "Intania Innovation Contest",
+            description:
+                "การแข่งขันนวัตกรรมนักเรียนระดับม้ธยมศึกษาตอนปลาย ในด้านวิศวกรรมศาสตร์ประจำปี 2568 ในธีม “Innovation towards Global Impact”",
+            image: "/arena/iic.png",
+            links: [
+                { name: "อ่านต่อ", url: "/" },
+            ],
+        },
+        {
+            title: "Intania Metaverse Exhibition",
+            description:
+                "Pitching รอบไฟนอลของ Thailand Metaverse Hackathon and Exhibition ครั้งที่ 1 และงานจัดแสดง Metaverse โดยบริษัทและหน่วยงานต่าง ๆ",
+            image: "/arena/ime.png",
+            links: [
+                { name: "อ่านต่อ", url: "/" },
+            ],
+        },
+    ];
+
     return (
         <div className="min-h-screen flex flex-col items-center justify-center">
             <div className="mt-32">
--- app/components/Arena/EventCard.tsx
+++ app/components/Arena/EventCard.tsx
@@ -2,43 +2,44 @@
 
 import React from "react";
 import { useRouter } from "next/navigation";
-import { motion } from "framer-motion";
+
+import Button from "../Button";
 
 const EventCard = ({ title, description, image, links }: any) => {
-    const router = useRouter();
+  const router = useRouter();
 
-    return (
-        <motion.div
-            initial={{ translateY: -50, opacity: 0 }}
-            animate={{ translateY: 0, opacity: 1, transition: { duration: 0.6, ease: "easeOut" } }}
-            exit={{ translateY: -50, opacity: 0, transition: { duration: 0.4, ease: "easeIn" } }}
-            className="w-[90vw] max-w-md flex flex-col justify-start items-start mb-6 transition-transform duration-300 hover:scale-105 cursor-pointer"
-            onClick={() => router.push(links[0].url)}
-        >
-            <img className="w-full h-48 object-cover" src={image} alt={title} />
-            <div className="w-full px-3.5 py-3 bg-stone-800 rounded-tl-lg flex flex-col justify-start items-start gap-1">
-                <div className="w-full text-white text-base font-bold font-['Archivo'] leading-normal tracking-tight">
-                    {title}
-                </div>
-                <div className="w-full text-white text-sm font-normal leading-tight tracking-tight font-['Chakra_Petch']">
-                    {description}
-                </div>
-                <div className="pt-2 inline-flex justify-start items-start gap-2">
-                    {links.map((link: any, index: number) => (
-                        <a
-                            key={index}
-                            href={link.url}
-                            className={`px-3.5 py-1.5 ${index === 0 ? "bg-stone-300 text-neutral-900 hover:bg-stone-500" : "outline outline-1 outline-offset-[-1px] outline-stone-300 text-white hover:bg-stone-300 hover:text-neutral-900"} text-sm font-bold flex justify-center items-center gap-2.5 overflow-hidden transition-colors duration-200`}
-                        >
-                            <div className="text-center justify-start leading-tight tracking-tight">
-                                {link.name}
-                            </div>
-                        </a>
-                    ))}
-                </div>
-            </div>
-        </motion.div>
-    );
+  return (
+    <div
+      className="w-[90vw] max-w-md flex flex-col justify-start items-start mb-6 transition-transform duration-300 hover:scale-105 cursor-pointer"
+      onClick={() => router.push(links[0].url)}
+    >
+      <img className="w-full h-48 object-cover" src={image} alt={title} />
+      <div className="w-full px-3.5 py-3 bg-stone-800 rounded-tl-lg flex flex-col justify-start items-start gap-1">
+        <div className="w-full text-white text-base font-bold font-['Archivo'] leading-normal tracking-tight">
+          {title}
+        </div>
+        <div className="w-full text-white text-sm font-normal leading-tight tracking-tight font-['Chakra_Petch']">
+          {description}
+        </div>
+        <div className="pt-2 inline-flex justify-start items-start gap-2">
+          {links.map((link: any, index: number) => (
+            <Button
+              key={index}
+              onClick={() => {
+                if (link.url) {
+                  router.push(link.url);
+                }
+              }}
+              appearance="filled"
+              className={`px-3.5 py-1.5 ${index === 0 ? "bg-stone-300 text-neutral-900 hover:bg-stone-500" : "outline outline-1 outline-offset-[-1px] outline-stone-300 text-white hover:bg-stone-300 hover:text-neutral-900"} text-sm font-bold flex justify-center items-center gap-2.5 overflow-hidden transition-colors duration-200`}
+            >
+              {link.name}
+            </Button>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default EventCard;
