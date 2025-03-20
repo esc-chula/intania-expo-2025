5e221445829667ecb59ca7d057fd8e3db41b648d
agehcx <chega.malee@gmail.com>
2025-03-20 19:04:01 +0700

    fix: indent

--- app/arena/page.tsx
+++ app/arena/page.tsx
@@ -4,66 +4,62 @@ import Icon from "../components/Icon";
 import TopAppBar from "../components/TopAppBar";
 
 export default function Events() {
-    const eventData = [
-        {
-            title: "Intania Hackathon",
-            description:
-                "เตรียมตัวพบกับ Intania Hackathon​ - งานที่จะสร้าง บรรยากาศที่ทำให้ทุกคนได้สร้างสิ่งใหม่ ๆ แก้ไขปัญหาใหม่ ๆ ที่สร้าง Impact ได้จริงไปด้วยกัน",
-            image: "/arena/ih.png",
-            links: [
-                { name: "ชมถ่ายทอดสด", url: "/" },
-                { name: "ติดตาม", url: "/" },
-            ],
-        },
-        {
-            title: "Integration Bee Challenge",
-            description:
-                "เตรียมตัวพบกับ Intania Hackathon​ - งานที่จะสร้าง บรรยากาศที่ทำให้ทุกคนได้สร้างสิ่งใหม่ ๆ แก้ไขปัญหาใหม่ ๆ ที่สร้าง Impact ได้จริงไปด้วยกัน",
-            image: "/arena/ib.png",
-            links: [
-                { name: "สมัคร", url: "/" },
-                { name: "อ่านต่อ", url: "/" },
-            ],
-        },
-        {
-            title: "Intania Innovation Contest",
-            description:
-                "การแข่งขันนวัตกรรมนักเรียนระดับม้ธยมศึกษาตอนปลาย ในด้านวิศวกรรมศาสตร์ประจำปี 2568 ในธีม “Innovation towards Global Impact”",
-            image: "/arena/iic.png",
-            links: [
-                { name: "อ่านต่อ", url: "/" },
-            ],
-        },
-        {
-            title: "Intania Metaverse Exhibition",
-            description:
-                "Pitching รอบไฟนอลของ Thailand Metaverse Hackathon and Exhibition ครั้งที่ 1 และงานจัดแสดง Metaverse โดยบริษัทและหน่วยงานต่าง ๆ",
-            image: "/arena/ime.png",
-            links: [
-                { name: "อ่านต่อ", url: "/" },
-            ],
-        },
-    ];
+  const eventData = [
+    {
+      title: "Intania Hackathon",
+      description:
+        "เตรียมตัวพบกับ Intania Hackathon​ - งานที่จะสร้าง บรรยากาศที่ทำให้ทุกคนได้สร้างสิ่งใหม่ ๆ แก้ไขปัญหาใหม่ ๆ ที่สร้าง Impact ได้จริงไปด้วยกัน",
+      image: "/arena/ih.png",
+      links: [
+        { name: "ชมถ่ายทอดสด", url: "/" },
+        { name: "ติดตาม", url: "/" },
+      ],
+    },
+    {
+      title: "Integration Bee Challenge",
+      description:
+        "เตรียมตัวพบกับ Intania Hackathon​ - งานที่จะสร้าง บรรยากาศที่ทำให้ทุกคนได้สร้างสิ่งใหม่ ๆ แก้ไขปัญหาใหม่ ๆ ที่สร้าง Impact ได้จริงไปด้วยกัน",
+      image: "/arena/ib.png",
+      links: [
+        { name: "สมัคร", url: "/" },
+        { name: "อ่านต่อ", url: "/" },
+      ],
+    },
+    {
+      title: "Intania Innovation Contest",
+      description:
+        "การแข่งขันนวัตกรรมนักเรียนระดับม้ธยมศึกษาตอนปลาย ในด้านวิศวกรรมศาสตร์ประจำปี 2568 ในธีม “Innovation towards Global Impact”",
+      image: "/arena/iic.png",
+      links: [{ name: "อ่านต่อ", url: "/" }],
+    },
+    {
+      title: "Intania Metaverse Exhibition",
+      description:
+        "Pitching รอบไฟนอลของ Thailand Metaverse Hackathon and Exhibition ครั้งที่ 1 และงานจัดแสดง Metaverse โดยบริษัทและหน่วยงานต่าง ๆ",
+      image: "/arena/ime.png",
+      links: [{ name: "อ่านต่อ", url: "/" }],
+    },
+  ];
 
-    return (
-        <div className="min-h-screen flex flex-col items-center justify-center">
-            <div className="mt-32">
-                <TopAppBar>
-                    <Button appearance="text">
-                        <Icon name="arrow_back" />
-                    </Button>
-                    <h1 className="text-2xl font-bold">Intania Arena</h1>
-                </TopAppBar>
-                {eventData.map((event, index) => (
-                    <EventCard
-                        key={index}
-                        title={event.title}
-                        description={event.description}
-                        image={event.image}
-                        links={event.links}
-                    />
-                ))}
-            </div>
-        </div>
-    );
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center">
+      <div className="mt-32">
+        <TopAppBar>
+          <Button appearance="text">
+            <Icon name="arrow_back" />
+          </Button>
+          <h1 className="text-2xl font-bold">Intania Arena</h1>
+        </TopAppBar>
+        {eventData.map((event, index) => (
+          <EventCard
+            key={index}
+            title={event.title}
+            description={event.description}
+            image={event.image}
+            links={event.links}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
--- app/components/Arena/EventCard.tsx
+++ app/components/Arena/EventCard.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import React from "react";
 import { useRouter } from "next/navigation";
@@ -10,18 +10,18 @@ const EventCard = ({ title, description, image, links }: any) => {
 
   return (
     <div
-      className="w-[90vw] max-w-md flex flex-col justify-start items-start mb-6 transition-transform duration-300 hover:scale-105 cursor-pointer"
+      className="mb-6 flex w-[90vw] max-w-md cursor-pointer flex-col items-start justify-start transition-transform duration-300 hover:scale-105"
       onClick={() => router.push(links[0].url)}
     >
-      <img className="w-full h-48 object-cover" src={image} alt={title} />
-      <div className="w-full px-3.5 py-3 bg-stone-800 rounded-tl-lg flex flex-col justify-start items-start gap-1">
-        <div className="w-full text-white text-base font-bold font-['Archivo'] leading-normal tracking-tight">
+      <img className="h-48 w-full object-cover" src={image} alt={title} />
+      <div className="flex w-full flex-col items-start justify-start gap-1 rounded-tl-lg bg-stone-800 px-3.5 py-3">
+        <div className="w-full font-['Archivo'] text-base leading-normal font-bold tracking-tight text-white">
           {title}
         </div>
-        <div className="w-full text-white text-sm font-normal leading-tight tracking-tight font-['Chakra_Petch']">
+        <div className="w-full font-['Chakra_Petch'] text-sm leading-tight font-normal tracking-tight text-white">
           {description}
         </div>
-        <div className="pt-2 inline-flex justify-start items-start gap-2">
+        <div className="inline-flex items-start justify-start gap-2 pt-2">
           {links.map((link: any, index: number) => (
             <Button
               key={index}
@@ -31,7 +31,7 @@ const EventCard = ({ title, description, image, links }: any) => {
                 }
               }}
               appearance="filled"
-              className={`px-3.5 py-1.5 ${index === 0 ? "bg-stone-300 text-neutral-900 hover:bg-stone-500" : "outline outline-1 outline-offset-[-1px] outline-stone-300 text-white hover:bg-stone-300 hover:text-neutral-900"} text-sm font-bold flex justify-center items-center gap-2.5 overflow-hidden transition-colors duration-200`}
+              className={`px-3.5 py-1.5 ${index === 0 ? "bg-stone-300 text-neutral-900 hover:bg-stone-500" : "text-white outline outline-1 outline-offset-[-1px] outline-stone-300 hover:bg-stone-300 hover:text-neutral-900"} flex items-center justify-center gap-2.5 overflow-hidden text-sm font-bold transition-colors duration-200`}
             >
               {link.name}
             </Button>
