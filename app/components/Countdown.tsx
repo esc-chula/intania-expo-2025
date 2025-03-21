3c173cae5979e00bfe6e1203d028d95852128c72
Siravit Phokeed <siravitsasa@gmail.com>
2025-03-22 01:25:42 +0700

    feat: Countdown

---
+++ app/components/Countdown.tsx
@@ -0,0 +1,57 @@
+"use client";
+
+import CountdownBox from "@/app/components/CountdownBox";
+import cn from "@/lib/helpers/cn";
+import { StyleableFC } from "@/lib/types/misc";
+import { list } from "radash";
+import { useEffect, useState } from "react";
+
+/**
+ * A countdown timer that displays the time remaining until a given date in
+ * days, hours, minutes, and seconds.
+ *
+ * @param date The date to count down to.
+ */
+const Countdown: StyleableFC<{
+  date: Date;
+}> = ({ date, className, style }) => {
+  const [now, setNow] = useState<Date>(new Date());
+  useEffect(() => {
+    const interval = setInterval(() => setNow(new Date()), 1000);
+    return () => clearInterval(interval);
+  }, []);
+
+  const diff = date.getTime() - now.getTime();
+  if (diff < 0)
+    return (
+      <section className={cn(`relative flex gap-2`, className)} style={style}>
+        <p
+          className={cn(`text-body-lg leading-body-lg absolute top-1/2 left-1/2
+            -translate-1/2`)}
+        >
+          งานเริ่มแล้ว!
+        </p>
+        {list(3).map((_, i) => (
+          <div aria-hidden key={i} className="bg-dark-red aspect-square w-15" />
+        ))}
+      </section>
+    );
+
+  // prettier-ignore
+  const segments = [
+    { value: Math.floor(diff / (1000 * 60 * 60 * 24)), max: 99, label: "days" },
+    { value: Math.floor((diff / (1000 * 60 * 60)) % 24), max: 24, label: "hrs" },
+    { value: Math.floor((diff / (1000 * 60)) % 60), max: 60, label: "min" },
+    { value: Math.floor((diff / 1000) % 60), max: 60, label: "sec" },
+  ];
+
+  return (
+    <ul role="list" className={cn(`flex gap-2`, className)} style={style}>
+      {segments.map((_, i) => (
+        <CountdownBox key={i} segments={segments} index={i} />
+      ))}
+    </ul>
+  );
+};
+
+export default Countdown;
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,7 @@
+import Button from "@/app/components/Button";
+import Countdown from "@/app/components/Countdown";
 import Logo from "@/app/components/Logo";
+import { EVENT_START_DATE } from "@/lib/config";
 import cn from "@/lib/helpers/cn";
 
 export default function Home() {
@@ -8,6 +11,20 @@ export default function Home() {
         text-center`)}
     >
       <Logo size={280} />
+      <section
+        className={cn(`text-cream leading-headline-sm text-headline-sm
+          -mt-3 space-y-2 italic [&_*]:block`)}
+      >
+        <time className="font-bold">28–30 March 2025</time>
+        <address className="-space-y-1">
+          <span>Faculty of Engineering</span>
+          <span>Chulalongkorn University</span>
+        </address>
+      </section>
+      <Countdown date={EVENT_START_DATE} className="mt-11" />
+      <Button appearance="tonal" className="mt-27">
+        สมัครเลย
+      </Button>
     </div>
   );
 }
