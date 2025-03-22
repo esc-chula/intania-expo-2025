ef315d88af20996fd21d879a85f10204ce1242da
DEC-48 <tanwajava@gmail.com>
2025-03-23 00:22:44 +0700

    feat : add CheckInExpo table for Visitor and fix  seed.ts, types/user

--- lib/backend/types/user.ts
+++ lib/backend/types/user.ts
@@ -22,7 +22,6 @@ export type Visitor = {
   gender: string;
   phone: string;
   category: string;
-  visitDate?: Date;
   interestedActivities?: string;
   referralSource?: string;
   studentLevel?: string;
