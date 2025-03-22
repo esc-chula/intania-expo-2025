fb9c92ed1cac4a3fdd2058f23d910b3e57a22ceb
Duckin99 <kittichon.not@gmail.com>
2025-03-22 22:41:52 +0700

    fix: getEventByID API (return type, unused try catch)

--- app/api/events/[id]/route.ts
+++ app/api/events/[id]/route.ts
@@ -1,28 +1,22 @@
 import { NextResponse } from "next/server";
 import { prisma, returnPrismaError } from "@/lib/backend/prisma";
 import { StatusCodes } from "http-status-codes";
+import { EventDetail } from "@/lib/backend/types/event";
+import { HTTPError } from "@/lib/backend/types/httpError";
 
 //Get an Event by ID
-export async function GET(request: Request, { params }: { params: Promise<{ id: string }> }) {
+export async function GET(request: Request, { params }: { params: Promise<{ id: string }> }): Promise<NextResponse<EventDetail | Record<string, EventDetail> | HTTPError>> {
     const {id} = await params;
 
-    try{
-        try {
-            const event = await prisma.event.findFirstOrThrow({
-            include: {
-                tags: true
-            },
-            where: {
-                id: id
-            },
-            });
-            return NextResponse.json(event, { status: StatusCodes.OK });
-        } catch (error) {
-            return returnPrismaError(error, [
-                { code: 'P2025', msg: 'Event not found', status: StatusCodes.NOT_FOUND },
-            ]);
-        }
+    try {
+        const event = await prisma.event.findFirstOrThrow({
+        include: {tags: true, intaniaLocation: true},
+        where: {id: id},
+        });
+        return NextResponse.json(event, { status: StatusCodes.OK });
     } catch (error) {
-        return NextResponse.json(error, {status: StatusCodes.INTERNAL_SERVER_ERROR});
+        return returnPrismaError(error, [
+            { code: 'P2025', msg: 'Event not found', status: StatusCodes.NOT_FOUND },
+        ]);
     }
 }
