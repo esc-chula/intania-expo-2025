bb6d57dc960a1a68096589231375d313ac76130d
Siravit Phokeed <siravitsasa@gmail.com>
2025-03-24 01:58:15 +0700

    feat: middleware

--- lib/models/User.ts
+++ lib/models/User.ts
@@ -22,6 +22,21 @@ export default class User {
   }
 
   /**
+   * Check if the authorized user has submitted the registration form. Uses
+   * `sixDigitCode` as the indicator under the hood.
+   *
+   * @param cookieStore The cookie store.
+   */
+  static async isRegistered(cookieStore: CookieStore) {
+    const { data, status, ok } = await Database.fetch<{
+      isRegistered: boolean;
+    }>("GET", "/auth/is-registered", undefined, {
+      headers: { Cookie: cookieStore.toString() },
+    });
+    return { response: data?.isRegistered || false, status, ok };
+  }
+
+  /**
    * Fetches the authorized user from client cookies.
    * @param cookieStore The cookie store.
    */
