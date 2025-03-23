d3e290c60b12ffe3f299a9634936d5457c45ea5f
Siravit Phokeed <siravitsasa@gmail.com>
2025-03-24 01:53:35 +0700

    fix: SignOutButton throws

--- app/home/components/SignOutButton.tsx
+++ app/home/components/SignOutButton.tsx
@@ -3,20 +3,24 @@
 import Button from "@/app/components/Button";
 import User from "@/lib/models/User";
 import { StyleableFC } from "@/lib/types/misc";
-import { redirect } from "next/navigation";
+import { useRouter } from "next/navigation";
 
-const SignOutButton: StyleableFC = ({ className, style }) => (
-  <Button
-    appearance="tonal"
-    onClick={async () => {
-      await User.signOut();
-      redirect("/");
-    }}
-    className={className}
-    style={style}
-  >
-    ออกจากระบบ
-  </Button>
-);
+const SignOutButton: StyleableFC = ({ className, style }) => {
+  const router = useRouter();
+
+  return (
+    <Button
+      appearance="tonal"
+      onClick={async () => {
+        await User.signOut();
+        router.push("/");
+      }}
+      className={className}
+      style={style}
+    >
+      ออกจากระบบ
+    </Button>
+  );
+};
 
 export default SignOutButton;
