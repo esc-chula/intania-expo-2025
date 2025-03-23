ed81fe1984d8b063ecc2041bdd275116a1af9d04
Siravit Phokeed <siravitsasa@gmail.com>
2025-03-24 00:30:50 +0700

    feat: User.fromCookies

--- lib/models/User.ts
+++ lib/models/User.ts
@@ -1,3 +1,6 @@
+import { cookies } from "next/headers";
+import Database, { DatabaseResponse } from "./Database";
+
 export enum UserRole {
   staff = "STAFF",
   visitor = "VISITOR",
@@ -10,6 +13,23 @@ export default class User {
   constructor(email: string, role: UserRole) {
     this.#email = email;
     this.#role = role;
+  }
+
+  /**
+   * Fetches the authorized user from client cookies.
+   * @param cookieStore The cookie store.
+   */
+  static async fromCookies(
+    cookieStore: Awaited<ReturnType<typeof cookies>>,
+  ): Promise<DatabaseResponse<User | null>> {
+    const { data, status, ok } = await Database.fetch<{
+      email: string;
+    }>("GET", "/visitors/me", undefined, {
+      headers: { Cookie: cookieStore.toString() },
+    });
+    let user: User | null = null;
+    if (ok) user = new User(data.email, UserRole.visitor);
+    return { data: user, status, ok } as DatabaseResponse<User | null>;
   }
 
   get email() {
