d48a1aabf1dd3b12108799b92419ef82f07b6115
Siravit Phokeed <siravitsasa@gmail.com>
2025-03-24 02:38:55 +0700

    fix: university year can be empty

--- lib/models/UniversityVisitor.ts
+++ lib/models/UniversityVisitor.ts
@@ -13,6 +13,7 @@ export default class UniversityVisitor extends Visitor {
     },
   ) {
     super(data);
+    if (!data.universityYear.length) throw new Error("Invalid university year");
     this.#universityYear = data.universityYear;
     this.#faculty = data.faculty;
     this.#university = data.university;
