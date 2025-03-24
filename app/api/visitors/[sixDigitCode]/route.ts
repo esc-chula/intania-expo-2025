a5e7d8b712256b726632426eb9c3d3804188981c
Duckin99 <kittichon.not@gmail.com>
2025-03-24 23:45:45 +0700

    fix: VisitorDetails

--- app/api/visitors/[sixDigitCode]/route.ts
+++ app/api/visitors/[sixDigitCode]/route.ts
@@ -10,6 +10,36 @@ export async function GET(request: Request, { params }: { params: Promise<{ sixD
     try {
         const user = await prisma.user.findFirstOrThrow({
             where: { sixDigitCode: sixDigitCode },
+            select: {
+                id: true,
+                email: true,
+                role: true,
+                sixDigitCode: true,
+                incrementCode: true,
+                name: true,
+                surname: true,
+                gender: true,
+                phone: true,
+                category: true,
+                visitDates: true,
+                interestedActivities: true,
+                referralSources: true,
+                studentLevel: true,
+                studyStream: true,
+                school: true,
+                province: true,
+                interestLevel: true,
+                interestedFields: true,
+                emergencyContact: true,
+                universityYear: true,
+                faculty: true,
+                university: true,
+                alumniBatch: true,
+                teacherSchool: true,
+                teacherProvince: true,
+                subjectTaught: true,
+                workshopId: false, // Exclude workshopId
+            },
         });
         return NextResponse.json(user, { status: StatusCodes.OK });
     } catch (error) {
