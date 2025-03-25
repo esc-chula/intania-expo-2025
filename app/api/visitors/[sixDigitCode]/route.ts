715f80fd507ef765d54424553f4c8bdc1d2f9f21
Ashira Aungsumal <i.deepaung@gmail.com>
2025-03-25 21:32:08 +0700

    feat: add middlewares in getVisitorBySixDigitCode API

--- app/api/visitors/[sixDigitCode]/route.ts
+++ app/api/visitors/[sixDigitCode]/route.ts
@@ -1,51 +1,81 @@
-import { NextResponse } from 'next/server';
-import { prisma, returnPrismaError } from '@/lib/backend/prisma';
-import { StatusCodes } from 'http-status-codes';
-import { VisitorDetails } from "@/lib/backend/types/user";
+import { isOneOfRole, onlyAuthorized } from "@/lib/backend/middleware";
+import { prisma, returnPrismaError } from "@/lib/backend/prisma";
 import { HTTPError } from "@/lib/backend/types/httpError";
+import { VisitorDetails } from "@/lib/backend/types/user";
+import { StatusCodes } from "http-status-codes";
+import { cookies } from "next/headers";
+import { NextResponse } from "next/server";
 
-export async function GET(request: Request, { params }: { params: Promise<{ sixDigitCode: string }> }): Promise<NextResponse<VisitorDetails | HTTPError>> {
-    const { sixDigitCode } = await params;
+export async function GET(
+  _: Request,
+  { params }: { params: Promise<{ sixDigitCode: string }> },
+): Promise<NextResponse<VisitorDetails | HTTPError>> {
+  const cookieStore = await cookies();
 
-    try {
-        const user = await prisma.user.findFirstOrThrow({
-            where: { sixDigitCode: sixDigitCode },
-            select: {
-                id: true,
-                email: true,
-                role: true,
-                sixDigitCode: true,
-                incrementCode: true,
-                name: true,
-                surname: true,
-                gender: true,
-                phone: true,
-                category: true,
-                visitDates: true,
-                interestedActivities: true,
-                referralSources: true,
-                studentLevel: true,
-                studyStream: true,
-                school: true,
-                province: true,
-                interestLevel: true,
-                interestedFields: true,
-                emergencyContact: true,
-                universityYear: true,
-                faculty: true,
-                university: true,
-                alumniBatch: true,
-                teacherSchool: true,
-                teacherProvince: true,
-                subjectTaught: true,
-                workshopId: false, // Exclude workshopId
-            },
-        });
-        return NextResponse.json(user, { status: StatusCodes.OK });
-    } catch (error) {
-        return returnPrismaError(error, [
-            { code: 'P2025', msg: 'visitor not found', status: StatusCodes.NOT_FOUND },
-            { code: 'P2023', msg: 'visitor not found', status: StatusCodes.NOT_FOUND },
-        ]);
-    }
+  const middlewareResponse = await onlyAuthorized(cookieStore);
+  if (!middlewareResponse.pass) {
+    return middlewareResponse.response!;
+  }
+  const { payload } = middlewareResponse.data!;
+  console.log(payload);
+
+  const middlewareResponse2 = isOneOfRole(
+    ["EXPO_STAFF", "WORKSHOP_STAFF"],
+    payload,
+  );
+  if (!middlewareResponse2.pass) {
+    return middlewareResponse2.response!;
+  }
+
+  const { sixDigitCode } = await params;
+
+  try {
+    const user = await prisma.user.findFirstOrThrow({
+      where: { sixDigitCode: sixDigitCode },
+      select: {
+        id: true,
+        email: true,
+        role: true,
+        sixDigitCode: true,
+        incrementCode: true,
+        name: true,
+        surname: true,
+        gender: true,
+        phone: true,
+        category: true,
+        visitDates: true,
+        interestedActivities: true,
+        referralSources: true,
+        studentLevel: true,
+        studyStream: true,
+        school: true,
+        province: true,
+        interestLevel: true,
+        interestedFields: true,
+        emergencyContact: true,
+        universityYear: true,
+        faculty: true,
+        university: true,
+        alumniBatch: true,
+        teacherSchool: true,
+        teacherProvince: true,
+        subjectTaught: true,
+        workshopId: false, // Exclude workshopId
+      },
+    });
+    return NextResponse.json(user, { status: StatusCodes.OK });
+  } catch (error) {
+    return returnPrismaError(error, [
+      {
+        code: "P2025",
+        msg: "visitor not found",
+        status: StatusCodes.NOT_FOUND,
+      },
+      {
+        code: "P2023",
+        msg: "visitor not found",
+        status: StatusCodes.NOT_FOUND,
+      },
+    ]);
+  }
 }
