708a7364389b4cb891a3f6b5edcdbd2482a02d5d
Siravit Phokeed <siravitsasa@gmail.com>
2025-03-26 05:02:14 +0700

    feat: standard TopAppBar

--- app/components/TopAppBar.tsx
+++ app/components/TopAppBar.tsx
@@ -13,8 +13,8 @@ import { useEffect, useState } from "react";
  */
 const TopAppBar: StyleableFC<{
   children: React.ReactNode;
-  appearance?: "filled" | "scrim" | "minimal";
-}> = ({ children, appearance = "filled", className, style }) => {
+  appearance?: "standard" | "scrim" | "minimal";
+}> = ({ children, appearance = "standard", className, style }) => {
   const [scrolled, setScrolled] = useState(false);
   useEffect(() => {
     const handleScroll = () =>
@@ -31,15 +31,15 @@ const TopAppBar: StyleableFC<{
         bottom-auto z-40 flex h-16 w-screen max-w-108 -translate-x-4
         items-center gap-2 px-2 text-white transition-colors [&_h1]:grow
         [&_h1]:truncate [&_h1]:font-bold [&_h1]:transition-opacity`,
-        {
-          filled: `bg-black`,
-          scrim: scrolled
-            ? `bg-black`
-            : `*:z-10 before:absolute before:inset-0 before:h-30
-              before:bg-gradient-to-b before:from-black before:to-transparent
-              before:content-['']`,
-          minimal: scrolled ? `bg-black` : `[&_h1]:opacity-0`,
-        }[appearance],
+        scrolled
+          ? `bg-black`
+          : {
+              standard: null,
+              scrim: `*:z-10 before:absolute before:inset-0 before:h-30
+                before:bg-gradient-to-b before:from-black before:to-transparent
+                before:content-['']`,
+              minimal: `[&_h1]:opacity-0`,
+            }[appearance],
         className,
       )}
       style={style}
