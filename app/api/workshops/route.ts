223657fdaef458d9d18f40642e60bf36e52a7c50
Ashira Aungsumal <i.deepaung@gmail.com>
2025-03-26 13:48:13 +0700

    fix: update everone can call getAllWorkshops API

--- app/api/workshops/route.ts
+++ app/api/workshops/route.ts
@@ -1,10 +1,8 @@
-import { onlyAuthorized } from "@/lib/backend/middleware";
 import { prisma } from "@/lib/backend/prisma";
 import { WorkshopQuerySchema } from "@/lib/backend/schemas/query";
 import { HTTPError } from "@/lib/backend/types/httpError";
 import { WorkshopDetail } from "@/lib/backend/types/workshop";
 import { ReasonPhrases, StatusCodes } from "http-status-codes";
-import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 import { fromZodError } from "zod-validation-error";
 
@@ -14,45 +12,6 @@ export async function GET(
   request: Request,
 ): Promise<NextResponse<WorkshopDetail[] | HTTPError>> {
   const { searchParams } = new URL(request.url);
-
-  const cookieStore = await cookies();
-
-  const middlewareResponse = await onlyAuthorized(cookieStore);
-  if (!middlewareResponse.pass) {
-    return middlewareResponse.response!;
-  }
-  const { payload } = middlewareResponse.data!;
-
-  // const middlewareResponse2 = isOneOfRole(["VISITOR" ], payload);
-  // if (!middlewareResponse2.pass) {
-  //   return middlewareResponse2.response!;
-  // } //no need
-
-  // get visitor id
-  const userResult = await prisma.user.findFirst({
-    where: { email: payload.email },
-    select: { id: true },
-  });
-  if (!userResult) {
-    // payload email should be valid
-    return NextResponse.json(
-      { error: ReasonPhrases.INTERNAL_SERVER_ERROR },
-      { status: StatusCodes.INTERNAL_SERVER_ERROR },
-    );
-  }
-  const visitorId = userResult.id;
-  const allRegisteredWorkshopSlotId =
-    await prisma.registeredWorkshopSlotOnVisitor.findMany({
-      where: {
-        visitorId: visitorId,
-      },
-      select: {
-        registeredWorkshopSlotId: true,
-      },
-    });
-  const setRegisteredWorkshopSlotId = new Set(
-    allRegisteredWorkshopSlotId.map((slot) => slot.registeredWorkshopSlotId),
-  );
 
   // Extract query parameters
   let data;
@@ -67,7 +26,6 @@ export async function GET(
 
   // Validate query parameters
   const validation = WorkshopQuerySchema.safeParse({ data: data });
-
   if (!validation.success) {
     return NextResponse.json(
       { error: fromZodError(validation.error).toString() },
@@ -83,31 +41,12 @@ export async function GET(
       include: { intaniaLocation: true, workshopSlots: true },
       where: {
         ...(search
-          ? {
-              OR: [{ name: { contains: search?.trim(), mode: "insensitive" } }],
-            }
+          ? { name: { contains: search?.trim(), mode: "insensitive" } }
           : {}),
       },
     });
 
-    const currentTime = new Date();
-    const processedWorkshops = workshops.map((workshop) => ({
-      ...workshop,
-      workshopSlots: workshop.workshopSlots.map((slot) => {
-        let status = "ว่าง"; // Default to available
-
-        if (setRegisteredWorkshopSlotId.has(slot.id)) {
-          status = "ลงทะเบียนแล้ว";
-        } else if (slot.currentRegistrantCount >= slot.maxRegistrantCount!) {
-          status = "เต็มแล้ว";
-        } else if (slot.endTime < currentTime) {
-          status = "ผ่านไปแล้ว";
-        }
-
-        return { ...slot, status };
-      }),
-    }));
-    return NextResponse.json(processedWorkshops, { status: StatusCodes.OK });
+    return NextResponse.json(workshops, { status: StatusCodes.OK });
   } catch (_) {
     return NextResponse.json(
       { error: ReasonPhrases.INTERNAL_SERVER_ERROR },
