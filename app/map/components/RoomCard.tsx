6c3bbe1fd27fa52b341758b032c265c676fcfc5b
Siravit Phokeed <siravitsasa@gmail.com>
2025-03-28 10:41:03 +0700

    fix: RoomCard name width not applied

--- app/map/components/RoomCard.tsx
+++ app/map/components/RoomCard.tsx
@@ -7,7 +7,9 @@ const RoomCard: StyleableFC<{ room: Room }> = ({ room, className, style }) => {
   return (
     <Card className={cn(`space-y-2 px-4 py-3`, className)} style={style}>
       <h3 className="text-title-md leading-title-md font-bold">
-        <span className={cn(room.event && `min-w-11 pr-3`)}>{room.name}</span>
+        <span className={cn(room.event && `inline-block min-w-14 pr-3`)}>
+          {room.name}
+        </span>
         {room.event && <span className="text-cream">{room.event}</span>}
       </h3>
       {room.body && <p className="text-body-md leading-body-md">{room.body}</p>}
