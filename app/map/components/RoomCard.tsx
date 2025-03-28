7daac4b985dbc0d760cde6eb3952104d1ca596c9
Siravit Phokeed <siravitsasa@gmail.com>
2025-03-28 10:39:27 +0700

    fix: RoomCard event wrapping behavior

--- app/map/components/RoomCard.tsx
+++ app/map/components/RoomCard.tsx
@@ -6,8 +6,8 @@ import { StyleableFC } from "@/lib/types/misc";
 const RoomCard: StyleableFC<{ room: Room }> = ({ room, className, style }) => {
   return (
     <Card className={cn(`space-y-2 px-4 py-3`, className)} style={style}>
-      <h3 className="text-title-md leading-title-md flex font-bold">
-        <span className={cn(room.event && `min-w-11 mr-3`)}>{room.name}</span>
+      <h3 className="text-title-md leading-title-md font-bold">
+        <span className={cn(room.event && `min-w-11 pr-3`)}>{room.name}</span>
         {room.event && <span className="text-cream">{room.event}</span>}
       </h3>
       {room.body && <p className="text-body-md leading-body-md">{room.body}</p>}
