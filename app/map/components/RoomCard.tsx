8d0c736ccc7509ef2ac3d7d034afaffbd1864674
Siravit Phokeed <siravitsasa@gmail.com>
2025-03-28 10:27:21 +0700

    fix: allow longer Room names

--- app/map/components/RoomCard.tsx
+++ app/map/components/RoomCard.tsx
@@ -7,7 +7,7 @@ const RoomCard: StyleableFC<{ room: Room }> = ({ room, className, style }) => {
   return (
     <Card className={cn(`space-y-2 px-4 py-3`, className)} style={style}>
       <h3 className="text-title-md leading-title-md flex font-bold">
-        <span className={cn(room.event && `w-14`)}>{room.name}</span>
+        <span className={cn(room.event && `min-w-11 mr-3`)}>{room.name}</span>
         {room.event && <span className="text-cream">{room.event}</span>}
       </h3>
       {room.body && <p className="text-body-md leading-body-md">{room.body}</p>}
