4c045ce0c2dbbd601254dd1aa1b24fa04a25cde6
Siravit Phokeed <siravitsasa@gmail.com>
2025-03-29 15:32:38 +0700

    feat: full Navigation

--- app/home/components/Navigation.tsx
+++ app/home/components/Navigation.tsx
@@ -5,13 +5,29 @@ import { StyleableFC } from "@/lib/types/misc";
 const Navigation: StyleableFC = ({ className, style }) => (
   <nav className={cn(`space-y-4`, className)} style={style}>
     <NavigationCard
+      title="Explore the map"
+      body="คณะวิศวะมีตึกอะไรบ้าง น่าตาเป็นยังไง มีโซนอะไรบ้าง มาดูกันเลย!"
+      href="/map"
+    />
+    <NavigationCard
+      title="Activity schedule"
+      body="เวทีกลางเรามีกิจกรรมตลอดทั้งวัน ทั้งสนุก ทั้งสาระ!"
+      href="/schedule"
+    />
+    <NavigationCard
       title="Intania Quest"
       body="เจาะลึกภาควิชาแบบจัดเต็มผ่านเวิร์กชอปสุดเข้มข้น!"
       href="/workshops"
     />
     <NavigationCard
-      title="Coming Soon"
-      body="ใกล้ถึงงานแล้ว ตื่นเต้นๆ ! อย่าลืมกลับมาดูทีหลังด้วยนะ"
+      title="Intania Arena"
+      body="เตรียมพบกับสุดยอด 5 การแข่งขันปลุกพลังแห่งนักวิศวะ!"
+      href="/arena"
+    />
+    <NavigationCard
+      title="Find your major"
+      body="แต่ละภาควิชาเรียนอะไร มีสาขาอะไรบ้าง มาเลือก “ดวงดาว” ที่ใช่กันเถอะ!"
+      href="/majors"
     />
   </nav>
 );
